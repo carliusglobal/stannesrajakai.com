f92ca3171d76e6ec0d359ecc4b0d72760230d7c9
Navis Michael Bearly J <michael.bearly@carliusglobal.com>
2019-11-09 13:25:04 +0530

    Adding static pages

--- application/config/config.php
+++ application/config/config.php
@@ -39,6 +39,7 @@ $config['base_url']    = "$root";
 | variable so that it is blank.
 |
 */
+//$config['index_page'] = 'index.php';
 $config['index_page'] = 'index.php';
 
 /*
---
+++ application/views/templates/header.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<title>Title</title>
+
+<link href="<?=asset_url();?>css/theme.css" rel="stylesheet" type="text/css" />
+<link href="<?=asset_url();?>css/woocommerce.css" rel="stylesheet" type="text/css" />
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+</head>
+<body>
+
+<header>
+	 <div class="tm-header-offset">  <!-- Start header offset-->
+		<div class="tm-head-a-box tm-medium-padding">
+			<div class="uk-container uk-container-center"> 						
+				<div class="uk-grid">
+					<!-- Start adress widget area -->
+					<div class="uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-3 tm-text-left">
+						<img width="65" height="70" src="<?=asset_url();?>images/greatlogo.png"/>
+					</div>
+					<!-- End adress widget area -->
+					<!-- Start top-logo widget area -->
+					<div class="uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-3 uk-text-center" style="font-size: 25px">
+						<br/>
+					    புனித&nbsp;அன்னம்மாள்&nbsp;ஆலயம்
+					    	<br/>
+					    	<br/>
+					    	St. Annes Church
+					</div>
+					<!-- End top-logo widget area -->
+					<!-- Start social-icons & search widget area -->
+					<div class="uk-width-small-1-1 uk-width-medium-1-3 uk-width-large-1-3 tm-text-right">
+						<div class="social-icons clearfix">
+							<div class=" uk-hidden-small tm-p-top-medium widget_text">
+								04652-267393 <br/> இரஜகை - 627114, திருநெல்வேலி <br/>  தூத்துக்குடி மறைமாவட்டம்   
+							</div>
+						</div>
+						<!-- End search widget area -->
+						<!-- End search widget area -->
+					</div>
+					<!-- End social-icons widget area -->
+				</div>
+			</div> 				
+		</div>
+		<div style="height: 72px;" class="uk-sticky-placeholder">
+			<div style="margin: 0px;" class="tm-fixed-menu" data-uk-sticky="">  <!-- Start fixed menu-->
+				<div class="tm-nav-a-box">				
+					<div class="uk-container uk-container-center">
+						<nav class="uk-navbar">
+							<!--tm-navbar (hide defolt styles) -->
+							<ul class="uk-navbar-nav uk-hidden-small">
+								<li class="uk-parent uk-active" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
+									<a href="/" class="">HOME</a>
+								</li>
+								<li class="uk-parent" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
+									<a href="/index.php/massandnovena" class="">MASS AND NOVENA</a></li>
+								<li class="uk-parent" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
+									<a href="event.html" class="">EVENT</a>
+									<div style="" class="uk-dropdown uk-dropdown-navbar uk-dropdown-width-1">
+										<div class="uk-grid uk-dropdown-grid">
+											<div class="uk-width-1-1">
+												<ul class="uk-nav uk-nav-navbar">
+													<li>
+														<a href="event-list.html" class="">EVENT LIST</a>
+													</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</li>
+									<li>
+										<a href="sermons.html" class="">SERMONS</a>
+									</li>
+									<li class="uk-parent" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
+										<a href="blog.html" class="">BLOG</a>
+										<div style="" class="uk-dropdown uk-dropdown-navbar uk-dropdown-width-1">
+											<div class="uk-grid uk-dropdown-grid">
+												<div class="uk-width-1-1">
+													<ul class="uk-nav uk-nav-navbar">
+														<li>
+															<a href="our-pastors.html" class="">OUR PASTORS</a>
+														</li>
+														<li>
+															<a href="testimonials.html" class="">TESTIMONIALS</a>
+														</li>
+														<li>
+															<a href="faq.html" class="">HELP</a>
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li>
+										<a href="gallery.html" class="">GALLERY</a>
+									</li>
+									<li>
+										<a href="shop.html" class="">SHOP</a>
+									</li>
+									<li>
+										<a href="contact-us.html" class="">CONTACT US</a>
+									</li>
+								</ul>						
+								<a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas=""></a>
+						</nav>
+					</div>
+				</div> 			
+			</div>
+		</div>  
+		<!-- End fixed menu-->
+		<div class="breadcrumbs">
+			<div class="uk-container uk-container-center breadcrumbs-box"></div>
+		</div>
+	</div>  <!-- End header offset-->		
+</header>
