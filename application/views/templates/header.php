377fc534966f399d0e5b3066c28adfa8d1c59923
Navis Michael Bearly J <michael.bearly@carliusglobal.com>
2019-11-09 13:46:11 +0530

    adding static pages

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -58,13 +58,13 @@
 								<li class="uk-parent" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
 									<a href="/index.php/massandnovena" class="">MASS AND NOVENA</a></li>
 								<li class="uk-parent" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
-									<a href="event.html" class="">EVENT</a>
+									<a href="/index.php/event" class="">EVENT</a>
 									<div style="" class="uk-dropdown uk-dropdown-navbar uk-dropdown-width-1">
 										<div class="uk-grid uk-dropdown-grid">
 											<div class="uk-width-1-1">
 												<ul class="uk-nav uk-nav-navbar">
 													<li>
-														<a href="event-list.html" class="">EVENT LIST</a>
+														<a href="/index.php/eventlist" class="">EVENT LIST</a>
 													</li>
 												</ul>
 											</div>
@@ -72,7 +72,7 @@
 									</div>
 								</li>
 									<li>
-										<a href="sermons.html" class="">SERMONS</a>
+										<a href="/index.php/sermons" class="">SERMONS</a>
 									</li>
 									<li class="uk-parent" data-uk-dropdown="{}" aria-haspopup="true" aria-expanded="false">
 										<a href="blog.html" class="">BLOG</a>
