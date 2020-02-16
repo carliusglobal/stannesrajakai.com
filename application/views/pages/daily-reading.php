092a914f7f18c5ebdef7ff8b489d64008e7ec681
Navis Michael Bearly J <michael.bearly@carliusglobal.com>
2020-02-16 16:25:20 +0530

    fix errors

--- application/views/pages/daily-reading.php
+++ application/views/pages/daily-reading.php
@@ -16,12 +16,9 @@ $file = "http://www.usccb.org/bible/readings/".date("mdy").".cfm";
 $content = file_get_contents($file);
 $first_step = explode( '<div id="contentarea" class="readings">' , $content );
 $second_step = explode("</div>" , $first_step[1] );
-echo "<pre>";
-print_r($second_step[4][1]);
-//print_r($second_step);
-
-
-
+echo "<pre>"
+print_r($first_step[0]);
+echo "</pre>"
 
 ?>
                     </div>
