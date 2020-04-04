15aacafbffc9d7a88552321966b68aa0c4a37c20
Navis Michael Bearly J <michael.bearly@gmail.com>
2020-04-04 22:15:55 +0530

    time conversion

--- application/views/pages/sundaymass.php
+++ application/views/pages/sundaymass.php
@@ -32,7 +32,6 @@
                             <h1 class="tm-page-title">குருத்து ஞாயிறு திருப்பலி</h1>
                         </div>
                         <div>5:30 am Sunday, Indian Standard Time (IST)</div>
-                        <br/>
                         <div>இந்திய நேரம் ஆதிகாலை 5:30 மணி </div>
 <iframe style=" width: 1060px;height: 600px;margin: 0 auto;background-color: #777;display:block;" src="https://www.youtube.com/embed/1XIA8JBPgNQ"  width="560" height="349" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <!--
