8b72d77d85e72e5582ea6ad86c82a23f0f1622b7
Navis Michael Bearly J <michael.bearly@gmail.com>
2020-04-04 21:05:43 +0530

    Palm sunday update

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -20,16 +20,16 @@
 													<div class="imageslider-background"><h3 class="slide-head">புனித அன்னம்மாள் ஆலயம், இரஜகை</h3></div><br>
 													<div style="padding: 10px;" class="imageslider-background"><h3 class="slide-head"></h3>
                                                     <!--இன்று November 10, 2019 பரலோக அன்னை விழி திறந்த விழாவை கொண்டாடும் இந்நன்னாளில் மதிய செப வழிபாடு (12:00 to 14:00 pm) மற்றும் தேர் பவனி மாலை 7:00 pm ஆடம்பர கூட்டு திருப்பலி & விருந்து -->
-                                                    குருத்து ஞாயிறு நேரலை - ஏசு கிறிஸ்துவின் பாடுகள் - 5 ஏப்ரல் 2020
+                                                    குருத்து ஞாயிறு நேரலை - இயேசு கிறிஸ்துவின் பாடுகள் - 5 ஏப்ரல் 2020
                                                     <br/>
                                                     <!--நிகழ்ச்சிகளை நேரலையில் நமது ஊர் websiteல் காண ஏற்பாடு செய்யப்பட்டுள்ளது.  -->
                                                     <br/>
-                                                    <a href="/index.php/sundaymass">நேரலை...</a>
+                                                    <a href="/index.php/sundaymass">குருத்து ஞாயிறு பதிவு </a>
                                                     <br/>
                                                     </div>
 
 												</div>
-												<a href="/index.php/livetv" class="tm-imageslider-button color-light">Click&nbsp;Here&nbsp;for&nbsp;Live&nbsp;telecast <i class="uk-icon-long-arrow-right"></i></a>
+												<a href="/index.php/livetv" class="tm-imageslider-button color-light">நேரலை...<i class="uk-icon-long-arrow-right"></i></a>
 											</div>
 										</div>
 									</div>
