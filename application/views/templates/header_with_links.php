92afd57af6092b75bb74a6824222e2e7534d576f
Navis Michael Bearly J <carliusglobal@gmail.com>
2022-03-27 15:38:06 +0530

    test commit

--- application/views/templates/header_with_links.php
+++ application/views/templates/header_with_links.php
@@ -29,7 +29,7 @@
 					    புனித&nbsp;அன்னம்மாள்&nbsp;ஆலயம்,&nbsp;இரஜகை
 					    	<br/>
 					    	<br/>
-					    	St. Annes Church, Rajakai
+					    	St. Annes Church, Rajakai - test
 					</div>
 					<!-- End top-logo widget area -->
 					<!-- Start social-icons & search widget area -->
